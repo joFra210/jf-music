291e677ab5a386aa3e07ead18ebeb9d582b243e8
Johannes Franze <joFra210@users.noreply.github.com>
2021-08-27 23:27:05 +0200

    startpage
    - add card component
    - add header
    - add lucky number generator

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: {
-    enabled: process.env.NODE_ENV === 'production' || false,
+    enabled: process.env.NODE_ENV === 'production',
     content: [
       './templates/**/*.twig',
       './templates/*.twig',
@@ -13,6 +13,14 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      mint: '#A7F3D0',
+      dark: '#3F3F37',
+      brown: '#A35F00',
+      capri: '#30C5FF',
+      mauve: '#F7B5F8',
+      white: '#FFFFFF',
+    }
   },
   variants: {
     extend: {},
